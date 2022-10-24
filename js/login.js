7803504d0552e278a4b5b9d16d6e43c9f15398f8
Bottonlina <alynne.daltoe@live.com>
2022-10-24 00:47:17 -0300

    first commit

---
+++ js/login.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
+
+const validateImput = ({ target }) => {
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled');
+        return;
+    }
+
+    button.setAttribute('disabled', '');
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value);
+    window.location = 'pages/game.html';
+}
+
+input.addEventListener('input', validateImput);
+form.addEventListener('submit', handleSubmit);
